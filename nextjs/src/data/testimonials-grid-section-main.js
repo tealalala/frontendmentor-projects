80f6882a95205a3aed9bb1af928ddcf0206ceafc
Tea La <tealalala@protonmail.com>
2021-03-01 20:46:42 -0800

    nextjs testimonials-grid-section-main: Organize this project before execution
    - /pages/testimonials-grid-section-main.js: Move testimonials data to /src/data/testimonials-grid-section-main.js
    - /src/data/testimonials-grid-section-main.js: Add testimonials data and export testimonials as a data object
    - /styles/Testimonials-grid-section-main.module.css: no changes

---
+++ nextjs/src/data/testimonials-grid-section-main.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    name: "Daniel Clifford",
+    subtitle: "Verified Graduate",
+    image_url: "/testimonials-grid-section-main/image-daniel.jpg",
+    pull_quote:
+      "I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.",
+    full_quote:
+      "I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup.",
+  },
+  {
+    name: "Jonathan Walters",
+    subtitle: "Verified Graduate",
+    image_url: "/testimonials-grid-section-main/image-jonathan.jpg",
+    pull_quote: "The team was very supportive and kept me motivated",
+    full_quote:
+      "I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself.",
+  },
+  {
+    name: "Jeanette Harmon",
+    subtitle: "Verified Graduate",
+    image_url: "/testimonials-grid-section-main/image-jeanette.jpg",
+    pull_quote: "An overall wonderful and rewarding experience",
+    full_quote:
+      "Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love.",
+  },
+  {
+    name: "Patrick Abrams",
+    subtitle: "Verified Graduate",
+    image_url: "/testimonials-grid-section-main/image-patrick.jpg",
+    pull_quote:
+      "Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.",
+    full_quote:
+      "The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people.",
+  },
+  {
+    name: "Kira Whittle",
+    subtitle: "Verified Graduate",
+    image_url: "/testimonials-grid-section-main/image-kira.jpg",
+    pull_quote: "Such a life-changing experience. Highly recommended!",
+    full_quote:
+      "Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend!",
+  },
+];
