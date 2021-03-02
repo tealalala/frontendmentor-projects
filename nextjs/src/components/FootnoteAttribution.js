55d94ff0e66401d675d695c87a659925a51f295f
Tea La <tealalala@protonmail.com>
2021-03-01 19:47:36 -0800

    nextjs: Add component FootnoteAttribution.js

---
+++ nextjs/src/components/FootnoteAttribution.js
@@ -0,0 +1,11 @@
+export default function FootnoteAttribution() {
+  return (
+    <div className="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://github.com/tealalala">Tea La</a>.
+    </div>
+  );
+}
