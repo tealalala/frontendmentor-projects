77e352bde3ece0962f8c5217efd0e4535cd91901
Tea La <tealalala@protonmail.com>
2021-03-01 19:39:24 -0800

    nextjs: Import to testimonials-grid-section-main.js its own module CSS

--- nextjs/pages/testimonials-grid-section-main.js
+++ nextjs/pages/testimonials-grid-section-main.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Testimonials-grid-section-main.module.css'
 
 export default function Home() {
   return (
