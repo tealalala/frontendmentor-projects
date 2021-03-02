13c2cde57c3b853a47835f00edd07a32c8d695e7
Tea La <tealalala@protonmail.com>
2021-03-02 06:19:31 -0800

    nextjs testimonials-grid-section-main: Add to styles/themes/ theme-testimonials-grid-section-main.js

---
+++ nextjs/styles/themes/theme-testimonials-grid-section-main.js
@@ -0,0 +1,12 @@
+const themeTestimomnialsGrid = {
+  colors: {
+    moderateViolet: 'hsl(263, 55%, 52%)',
+    veryDarkGrayishBlue: 'hsl(217, 19%, 35%)',
+    veryDarkBlackishBlue: 'hsl(219, 29%, 14%)',
+    white: 'hsl(0, 0%, 100%)',
+    lightGray: 'hsl(0, 0%, 81%)',
+    lightGrayishBlue: 'hsl(210, 46%, 95%)'
+  }
+}
+
+export default themeTestimomnialsGrid
