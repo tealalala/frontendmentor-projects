83aa1447965f49ab6ce64271f9320318e379c6ab
Tea La <tealalala@protonmail.com>
2021-03-08 12:10:39 -0800

    nextjs testimonials-grid-section-main: Add media queries template

--- nextjs/pages/testimonials-grid-section-main.js
+++ nextjs/pages/testimonials-grid-section-main.js
@@ -142,6 +142,18 @@ export default function Home() {
           grid-area: side-1;
           background-color: ${theme.colors.white};
         }
+
+        // Extra small devices (portrait phones, less than 576px)
+        @media (max-width: 575.98px) { ... }
+
+        // Small devices (landscape phones, less than 768px)
+        @media (max-width: 767.98px) { ... }
+
+        // Medium devices (tablets, less than 992px)
+        @media (max-width: 991.98px) { ... }
+
+        // Large devices (desktops, less than 1200px)
+        @media (max-width: 1199.98px) { ... }
       `}</style>
     </div>
   );
