a75f99d77b907c7383071f3e1885f7417410caf9
Tea La <tealalala@protonmail.com>
2021-03-08 13:51:11 -0800

    nextjs: Add to Avatar image alt tag

--- nextjs/src/components/Avatar.js
+++ nextjs/src/components/Avatar.js
@@ -5,7 +5,7 @@ export default function Avatar({name, subtitle, imageUrl}) {
   return (
     <div className="avatar">
       <div className="image">
-        <Image src={imageUrl} width='40px' height='40px' />
+        <Image src={imageUrl} width='40px' height='40px' alt={name + ", " + subtitle} />
       </div>
       <div className="details">
         <p className="name">{name}</p>
