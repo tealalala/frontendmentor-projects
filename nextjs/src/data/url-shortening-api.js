b58e6e0a9e438424faaf90a483f97e6fb5305345
Tea La <tealalala@protonmail.com>
2021-03-13 21:37:59 -0800

    nextjs url-shortening-api: Add data file

---
+++ nextjs/src/data/url-shortening-api.js
@@ -0,0 +1,124 @@
+export default [
+  navigation_left: {
+    {
+      title: "Features",
+      url: "/features"
+    },
+    {
+      title: "Pricing",
+      url: "/pricing"
+    },
+    {
+      title: "Resources",
+      url: "/resources"
+    }
+  },
+  navigation_right: {
+    {
+      title: "Login",
+      url: "/login"
+    },
+    {
+      title: "Sign up",
+      url: "/sign-up"
+    }
+  },
+  main_header: {
+    title: "More than just shorter links",
+    paragraph: "Build your brand’s recognition and get detailed insights on how your links are performing.",
+    cta_title: "Get Started",
+    cta_link: "/get-started"
+  },
+  body_content: {
+    title: "Advanced Statistics",
+    paragraph: "Track how your links are performing across the web with our advanced statistics dashboard."
+  }
+  body_card: {
+    {
+      title: "Brand Recognition",
+      paragraph: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+      image: "/url-shortening-api/icon-brand-recognition.svg"
+    },
+    {
+      title: "Detailed Records",
+      paragraph: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+      image: "/url-shortening-api/icon-detailed-records.svg"
+    },
+    {
+      title: "Fully Customizable",
+      paragraph: "Improve brand awareness and content discoverability through customizable  links, supercharging audience engagement.",
+      image: "/url-shortening-api/icon-full-customizable.svg"
+    }
+  },
+  footer_cta: {
+    title: "Boost your links today",
+    cta_title: "Get Started",
+    cta_link: "/get-started"
+  },
+  footer: {
+    features: {
+      {
+        title: "Link Shortening",
+        url: "/link-shortening"
+      },
+      {
+        title: "Branded Links",
+        url: "/branded-links"
+      },
+      {
+        title: "Analytics",
+        url: "/analytics"
+      },
+    },
+    resources: {
+      {
+        title: "Blog",
+        url: "/blog"
+      },
+      {
+        title: "Developers",
+        url: "/developers"
+      },
+      {
+        title: "Support",
+        url: "/support"
+      }
+    },
+    company: {
+      {
+        title: "About",
+        url: "/about"
+      },
+      {
+        title: "Our Team",
+        url: "/team"
+      },
+      {
+        title: "Careers",
+        url: "/careers"
+      },
+      {
+        title: "Contact",
+        url: "/contact"
+      }
+    },
+    social: {
+      {
+        image: "/url-shortening-api/icon-facebook.svg",
+        url: "/facebook"
+      },
+      {
+        image: "/url-shortening-api/icon-twitter.svg",
+        url: "/twitter"
+      },
+      {
+        image: "/url-shortening-api/icon-pinterest.svg",
+        url: "/pinterest"
+      },
+      {
+        image: "/url-shortening-api/icon-instagram.svg",
+        url: "/instagram"
+      }
+    }
+  }
+];
