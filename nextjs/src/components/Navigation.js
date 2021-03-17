9897893b2f55c9736c43e13cc39d0794c04c15cf
Tea La <tealalala@protonmail.com>
2021-03-16 19:42:18 -0700

    nextjs url-shortening-api: Update to className

--- nextjs/src/components/Navigation.js
+++ nextjs/src/components/Navigation.js
@@ -1,11 +1,11 @@
-export default function Navigation() {
+export default function Navigation({ data_company, data_nav_left, data_nav_right }) {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
         Navbar w/ text
       </a>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarText"
@@ -13,27 +13,27 @@ export default function Navigation() {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">
-              Home <span class="sr-only">(current)</span>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               Features
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               Pricing
             </a>
           </li>
         </ul>
-        <span class="navbar-text">Navbar text with an inline element</span>
+        <span className="navbar-text">Navbar text with an inline element</span>
       </div>
     </nav>
   );
