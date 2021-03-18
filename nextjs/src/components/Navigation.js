9adc39374a82c63806b4cc659ff9a4e91597a006
Tea La <tealalala@protonmail.com>
2021-03-17 20:10:23 -0700

    nextjs url-shortening-api: Add to navigation component a container

--- nextjs/src/components/Navigation.js
+++ nextjs/src/components/Navigation.js
@@ -1,40 +1,42 @@
 export default function Navigation({ data_company, data_nav_left, data_nav_right }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href={data_company.url}>
-        {data_company.name}
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarText"
-        aria-controls="navbarText"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarText">
-        <ul className="navbar-nav mr-auto">
-          {data_nav_left.map((item, index) => (
-            <li className="nav-item">
-              <a className="nav-link" href={item.url}>
-                {item.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <ul className="nav justify-content-end">
-          {data_nav_right.map((item, index) => (
-            <li className="nav-item">
-              <a className="nav-link" href={item.url}>
-                {item.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-        {/* <span className="navbar-text">Navbar text with an inline element</span> */}
+      <div class="container">
+        <a className="navbar-brand" href={data_company.url}>
+          {data_company.name}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+            {data_nav_left.map((item, index) => (
+              <li className="nav-item">
+                <a className="nav-link" href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <ul className="nav justify-content-end">
+            {data_nav_right.map((item, index) => (
+              <li className="nav-item">
+                <a className="nav-link" href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+          {/* <span className="navbar-text">Navbar text with an inline element</span> */}
+        </div>
       </div>
     </nav>
   );
