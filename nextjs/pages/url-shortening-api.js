c17bb13cae8b2930b942f37d85af56773dba1835
Tea La <tealalala@protonmail.com>
2021-03-17 20:47:39 -0700

    nextjs url-shortening-api: Add theme file

--- nextjs/pages/url-shortening-api.js
+++ nextjs/pages/url-shortening-api.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import theme from "../styles/themes/theme-testimonials-grid-section.js";
+import theme from "../styles/themes/theme-url-shortening-api.js";
 
 import data from '../src/data/url-shortening-api.js'
 
@@ -23,209 +23,17 @@ export default function Home() {
 
   return (
     <>
-      <Navigation 
+      <Navigation
         data_company={dataCompany}
         data_nav_left={dataNavLeft}
         data_nav_right={dataNavRight}
       />
-      <Hero 
-        data_hero={dataHero}
-      />
+      <Hero data_hero={dataHero} />
       <section className="container">
-        <div className="url-shortening-api">
-          {/* add content */}
-        </div>
+        <div className="url-shortening-api">{/* add content */}</div>
         <FootnoteAttribution frontendmentor_challenge_name="URL shortening API landing page" />
         <style global jsx>{`
-          @import url("https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@500;600&display=swap");
-
-          .testimonials-grid-section-main {
-            display: grid;
-            // grid-template-columns: auto auto auto auto;
-            grid-template-columns: 0.85fr 1fr 0.8fr 0.8fr;
-            grid-template-rows: auto auto;
-            gap: 24px 24px;
-            grid-template-areas:
-              "top-1 top-1 top-2 side-1"
-              "bottom-1 bottom-2 bottom-2 side-1";
-            padding: 32px 0px;
-            font-family: ${theme.body.fontFamily};
-            font-size: ${theme.body.fontSize};
-            font-weight: ${theme.body.fontWeightNormal};
-          }
-
-          .avatar {
-            display: flex;
-            padding-bottom: 16px;
-          }
-
-          .avatar .image {
-            padding-right: 16px;
-          }
-
-          .avatar img {
-            border-radius: 50%;
-          }
-
-          .avatar p {
-            margin-bottom: 0px !important;
-          }
-
-          .card {
-            padding: 24px;
-            border-radius: 12px;
-            border: 0px;
-          }
-
-          .card-0 .name,
-          .card-1 .name,
-          .card-3 .name,
-          .card-0 .pull-quote,
-          .card-1 .pull-quote,
-          .card-3 .pull-quote {
-            color: ${theme.colors.white};
-          }
-
-          .card-0 .subtitle,
-          .card-1 .subtitle,
-          .card-3 .subtitle {
-            color: ${theme.colors.lightGray};
-            opacity: 0.5;
-          }
-
-          .card-0 .full-quote,
-          .card-1 .full-quote,
-          .card-3 .full-quote {
-            color: ${theme.colors.white};
-            opacity: 0.7;
-          }
-
-          .card-2 .name,
-          .card-4 .name,
-          .card-2 .pull-quote,
-          .card-4 .pull-quote {
-            color: ${theme.colors.veryDarkBlackishBlue};
-          }
-
-          .card-2 .subtitle,
-          .card-4 .subtitle {
-            color: ${theme.colors.veryDarkGrayishBlue};
-            opacity: 0.5;
-          }
-
-          .card-2 .full-quote,
-          .card-4 .full-quote {
-            color: ${theme.colors.veryDarkGrayishBlue};
-            opacity: 0.7;
-          }
-
-          .card-0 .pull-quote,
-          .card-1 .pull-quote,
-          .card-2 .pull-quote,
-          .card-3 .pull-quote,
-          .card-4 .pull-quote {
-            padding-bottom: 16px;
-            font-weight: 600;
-            font-size: 20px;
-            line-height: 28px;
-          }
-
-          .card.card-0 {
-            grid-area: top-1;
-            background: ${theme.colors.moderateViolet}
-              url("/testimonials-grid-section/bg-pattern-quotation.svg")
-              no-repeat right 27% top 0%;
-            background-size: 20%;
-          }
-
-          .card.card-1 {
-            grid-area: top-2;
-            background-color: ${theme.colors.veryDarkGrayishBlue};
-          }
-
-          .card.card-2 {
-            grid-area: bottom-1;
-            background-color: ${theme.colors.white};
-          }
-
-          .card.card-3 {
-            grid-area: bottom-2;
-            background-color: ${theme.colors.veryDarkBlackishBlue};
-          }
-
-          .card.card-4 {
-            grid-area: side-1;
-            background-color: ${theme.colors.white};
-          }
-
-          .card.card-0 .avatar img {
-            border: 2px solid #ae90df !important;
-          }
-
-          .card.card-3 .avatar img {
-            border: 2px solid ${theme.colors.moderateViolet} !important;
-          }
-
-          .card.card-1 .avatar img,
-          .card.card-2 .avatar img,
-          .card.card-4 .avatar img {
-            border: 2px solid ${theme.colors.white} !important;
-          }
-
-          .card.card-2,
-          .card.card-4 {
-            box-shadow: 1rem 1rem 8rem -1rem ${theme.colors.lightGray};
-          }
-
-          // Extra small devices (portrait phones, less than 576px)
-          @media (max-width: 576px) {
-          }
-
-          // Small devices (landscape phones, less than 768px)
-          @media (max-width: 768px) {
-            .testimonials-grid-section-main {
-              display: flex;
-              flex-direction: column;
-            }
-            .card.card-0 {
-              background-repeat: no-repeat;
-              background-size: right 27% top 0%;
-              background-size: 20%;
-            }
-          }
-
-          // Medium devices (tablets, less than 992px)
-          @media (max-width: 992px) {
-            .testimonials-grid-section-main {
-              grid-template-columns: 0.9fr 1fr 0.9fr;
-              grid-template-rows: auto auto auto;
-              gap: 24px 24px;
-              grid-template-areas:
-                "top-1 top-1 top-2"
-                "middle-1 middle-2 middle-2"
-                "bottom-1 bottom-1 bottom-1";
-              padding: 32px 0px;
-            }
-            .card.card-0 {
-              grid-area: top-1;
-            }
-            .card.card-1 {
-              grid-area: top-2;
-            }
-            .card.card-2 {
-              grid-area: middle-1;
-            }
-            .card.card-3 {
-              grid-area: middle-2;
-            }
-            .card.card-4 {
-              grid-area: bottom-1;
-            }
-          }
-
-          // Large devices (desktops, less than 1200px)
-          @media (max-width: 1200px) {
-          }
+          @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap");
         `}</style>
       </section>
     </>
---
+++ nextjs/styles/themes/theme-url-shortening-api.js
@@ -0,0 +1,19 @@
+const themeUrlShorteningApi = {
+  body: {
+    fontFamily: 'Poppins',
+    fontSize: '18px',
+    fontWeightNormal: '500',
+    fontWeightMedium: '700'
+  },
+  colors: {
+    cyan: 'hsl(180, 66%, 49%)',
+    darkViolet: 'hsl(257, 27%, 26%)',
+    red: 'hsl(0, 87%, 67%)',
+    gray: 'hsl(0, 0%, 75%)',
+    GrayishViolet: 'hsl(257, 7%, 63%)',
+    veryDarkBlue: 'hsl(255, 11%, 22%)',
+    veryDarkViolet: 'hsl(260, 8%, 14%)'
+  }
+}
+
+export default themeUrlShorteningApi;
